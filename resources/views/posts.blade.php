01a131409fb12659b537f7409f614a47531eb3bd
Mohammad Rafa Adila <mohammadrafa63@gmail.com>
2022-11-22 22:25:35 +0700

    Belajar Laravel 8 | 7. Post Model (Selesai)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,15 +2,12 @@
 
 @section('container')
     
-    <h1>Halaman Blog Posts</h1>
+    <h1 class="mb-5">Halaman Blog Posts</h1>
 
     @foreach ($posts as $post)
-        <article class="mb-5">
-            <h2>
-                <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
-            </h2>
-            <h5>By: {{ $post["author"] }}</h5>
-            <p>{{ $post["body"] }}</p>
+        <article>
+            <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+            <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
 
