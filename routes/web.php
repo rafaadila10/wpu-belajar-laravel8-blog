d338848d64a1e30f7f5b71b3bb9092964431f76f
Mohammad Rafa Adila <mohammadrafa63@gmail.com>
2022-11-25 11:25:28 +0700

    Belajar Laravel 8 | 8. Post Category & Eloquent Relationship (selesai)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,3 +33,18 @@ Route::get('/about', function () {
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('category', [
+        'title' => $category->title,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
